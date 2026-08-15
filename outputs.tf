2cd0b5e5b47ec602f699fab7f5396ae48734333d
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:17 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,12 +4,12 @@ output "arc_kubernetes_flux_configurations_id" {
 }
 output "arc_kubernetes_flux_configurations_blob_storage" {
   description = "Map of blob_storage values across all arc_kubernetes_flux_configurations, keyed the same as var.arc_kubernetes_flux_configurations"
-  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => v.blob_storage if v.blob_storage != null && length(v.blob_storage) > 0 }
+  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => one(v.blob_storage) if v.blob_storage != null && length(v.blob_storage) > 0 }
   sensitive   = true
 }
 output "arc_kubernetes_flux_configurations_bucket" {
   description = "Map of bucket values across all arc_kubernetes_flux_configurations, keyed the same as var.arc_kubernetes_flux_configurations"
-  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => v.bucket if v.bucket != null && length(v.bucket) > 0 }
+  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => one(v.bucket) if v.bucket != null && length(v.bucket) > 0 }
   sensitive   = true
 }
 output "arc_kubernetes_flux_configurations_cluster_id" {
@@ -22,7 +22,7 @@ output "arc_kubernetes_flux_configurations_continuous_reconciliation_enabled" {
 }
 output "arc_kubernetes_flux_configurations_git_repository" {
   description = "Map of git_repository values across all arc_kubernetes_flux_configurations, keyed the same as var.arc_kubernetes_flux_configurations"
-  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => v.git_repository if v.git_repository != null && length(v.git_repository) > 0 }
+  value       = { for k, v in azurerm_arc_kubernetes_flux_configuration.arc_kubernetes_flux_configurations : k => one(v.git_repository) if v.git_repository != null && length(v.git_repository) > 0 }
   sensitive   = true
 }
 output "arc_kubernetes_flux_configurations_kustomizations" {
